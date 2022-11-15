b1c284ee916cd069f6889631e77e34982f3a484b
Ramtin <Ramtin.abadi@gmail.com>
2022-11-15 09:30:38 +0530

    Improved the comments

--- log.go
+++ log.go
@@ -5,30 +5,40 @@ import (
 	"strings"
 )
 
+var (
+	red    = "red"
+	green  = "green"
+	yellow = "yellow"
+)
+
+// Adds color to the given text
 func PaintText(text string, color string) string {
 	reset := "\033[0m"
-	red := "\033[31m"
-	green := "\033[32m"
-	yellow := "\033[33m"
-	if strings.ToLower(color) == "red" {
-		return red + text + reset
-	} else if strings.ToLower(color) == "green" {
-		return green + text + reset
-	} else if strings.ToLower(color) == "yellow" {
-		return yellow + text + reset
+	redC := "\033[31m"
+	greenC := "\033[32m"
+	yellowC := "\033[33m"
+	if strings.ToLower(color) == red {
+		return redC + text + reset
+	} else if strings.ToLower(color) == green {
+		return greenC + text + reset
+	} else if strings.ToLower(color) == yellow {
+		return yellowC + text + reset
 	}
 
 	return text
 }
 
+// Prints a new line with the given color
 func PrintColorln(text string, color string) {
 	fmt.Println(PaintText(text, color))
 }
 
+// Prints a new line with the color green
 func PrintSuccess(text string) {
 	PrintColorln(text, "green")
 }
 
+// Prints a new line with the color red
 func PrintError(text string) {
 	PrintColorln(text, "red")
 }
