5b1906731de9bb6577cd1b0147d258a845c3e188
Ramtin <Ramtin.abadi@gmail.com>
2024-04-03 22:23:18 +0300

    Version Bump to 0.2.2

---
+++ table.go
@@ -0,0 +1,82 @@
+package terminalutils
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+// This component uses BubbleTea's table
+// https://github.com/charmbracelet/bubbletea/tree/master/examples/table
+
+var tableBaseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
+
+type tableModel struct {
+	table      table.Model
+	OnRowClick func(table.Row) string
+}
+
+func (m tableModel) Init() tea.Cmd { return nil }
+
+func (m tableModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		case "enter":
+			return m, tea.Batch(
+				tea.Println(m.OnRowClick(m.table.SelectedRow())),
+			)
+		}
+	}
+	m.table, cmd = m.table.Update(msg)
+	return m, cmd
+}
+
+func (m tableModel) View() string {
+	return tableBaseStyle.Render(m.table.View()) + "\n"
+}
+
+type TableData struct {
+	Columns    []table.Column
+	Rows       []table.Row
+	OnRowClick func(table.Row) string
+}
+
+func Table(data TableData) {
+
+	t := table.New(
+		table.WithColumns(data.Columns),
+		table.WithRows(data.Rows),
+		table.WithFocused(true),
+		table.WithHeight(7),
+	)
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+	t.SetStyles(s)
+
+	m := tableModel{
+		table:      t,
+		OnRowClick: data.OnRowClick,
+	}
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+}
