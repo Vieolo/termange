aeffa66c64a424f48fc47463899f9dc53a87a9ce
Ramtin <Ramtin.abadi@gmail.com>
2025-12-23 23:29:16 +0100

    Improved the cursor functions

--- cursor/cursor.go
+++ cursor/cursor.go
@@ -6,16 +6,49 @@ import (
 	"github.com/vieolo/termange/internal"
 )
 
-// Internal terminal
+// The Cursor struct
+// This struct has no field, can be used for its functions
 type Cursor struct{}
 
-func (c Cursor) MoveUp(line int) Cursor {
-	for range line {
+// Moves the cursor up n times
+func (c Cursor) MoveUp(n int) Cursor {
+	for range n {
 		fmt.Print(internal.CursorUp)
 	}
 	return c
 }
 
+// Moves the cursor down n times
+func (c Cursor) MoveDown(n int) Cursor {
+	for range n {
+		fmt.Print(internal.CursorDown)
+	}
+	return c
+}
+
+// Moves the cursor right n times
+func (c Cursor) MoveRight(n int) Cursor {
+	for range n {
+		fmt.Print(internal.CursorRight)
+	}
+	return c
+}
+
+// Moves the cursor left n times
+func (c Cursor) MoveLeft(n int) Cursor {
+	for range n {
+		fmt.Print(internal.CursorLeft)
+	}
+	return c
+}
+
+// Moves the cursor to the start of the line
+func (c Cursor) MoveToLineStart() Cursor {
+	fmt.Print(internal.CursorToColumn0)
+	return c
+}
+
+// Clears the current line
 func (c Cursor) ClearLine() Cursor {
 	fmt.Print(internal.ClearLine)
 	return c
